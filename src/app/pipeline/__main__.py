c253774b7cdd51e4f14cdb4de0fbc435a60d7883
Apiwat Chantawibul <billiska@gmail.com>
2025-08-22 11:56:29 +0700

    implement pipeline runner
    - add logging to CLI

--- src/app/pipeline/__main__.py
+++ src/app/pipeline/__main__.py
@@ -1,6 +1,12 @@
+import logging
+
 from .cli import cli
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        format = '%(asctime)s %(name)s %(levelname)s: %(message)s',
+        level = logging.INFO,
+    )
     cli(prog_name = 'anon-runner')
 
--- src/app/pipeline/cli.py
+++ src/app/pipeline/cli.py
@@ -1,8 +1,11 @@
+import logging
+_logger = logging.getLogger(__name__)
 import pprint
 
 import click
 import devtools
 
+from app.dataset.view.schema import get_dataset_schema
 from .pipeline import Pipeline
 
 
@@ -24,7 +27,31 @@ def cli():
 @pipeline_file_argument
 def run(pipeline):
     """Run anonymization pipeline."""
-    pprint.pprint(pipeline)
+    if pipeline.load is None:
+        _logger.error('Pipeline has no loader.')
+        return
+
+    if pipeline.export is None:
+        _logger.error('Pipeline has no exporter.')
+        return
+
+    _logger.info(f'Loading dataset from: {pipeline.load.model_dump_json()}')
+    ds = pipeline.load()
+    print('Loaded dataset schema:')
+    devtools.pprint(get_dataset_schema(ds).model_dump(mode = 'json'))
+    print('Loaded dataset:')
+    print(ds)
+
+    _logger.info('Transforming dataset')
+    ds = pipeline.transform(ds)
+    print('Transformed dataset schema:')
+    devtools.pprint(get_dataset_schema(ds).model_dump(mode = 'json'))
+    print('Transformed dataset:')
+    print(ds)
+
+    _logger.info(f'Exporting dataset to: {pipeline.load.model_dump_json()}')
+    pipeline.export(ds)
+    _logger.info('Pipeline ran successfully')
 
 
 @cli.command()
@@ -36,6 +63,7 @@ def run(pipeline):
         'python-object', 'pyobj',
     ]),
     default = 'pydict',
+    show_default = True,
     help = 'Select output format. `py*` format are pretty-printed.',
 )
 @pipeline_file_argument
